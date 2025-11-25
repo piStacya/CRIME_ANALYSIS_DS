2603f7586a6bf98d6772e1497b37b83f59c85e34
user.kirkekisand <kirke.kisand@ut.ee>
2025-11-25 15:07:01 +0200

    legal cases proccessed since 2022 file to .tsv

--- Scripts/first_column_separation.py
+++ Scripts/first_column_separation.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 # Read the tab-separated file
 #df = pd.read_csv("filepath", sep="\t")
-df = pd.read_csv("C:\\Users\\panda\\Desktop\\sksksk and i oop\\kool\\andmeteadus\\projekt\\originalDatabases\\suspects_and_offenders_by_sex.tsv", sep="\t")
-
+df = pd.read_csv("C:\\Users\\panda\\Desktop\\sksksk and i oop\\kool\\andmeteadus\\projekt\\cleanedDatasetsCountryNames\\legal_cases_processed_in_first_instance_courts_by_legal_status_of_the_court_process_since2022.tsv", sep="\t")
 
 # Split the first column (which has comma-separated values)
 split_cols = df.iloc[:, 0].str.split(",", expand=True)
@@ -17,7 +16,7 @@ df = pd.concat([split_cols, df.iloc[:, 1:]], axis=1)
 
 # Save the cleaned version, iga tabeli jaoks norm nimi panna
 #df.to_csv("new file name", sep="\t", index=False)
-df.to_csv("suspects_and_offenders_by_sex.tsv", sep="\t", index=False)
+df.to_csv("legal_cases_processed_in_first_instance_courts_by_legal_status_of_the_court_process_since2022.tsv", sep="\t", index=False)
 
 print("Cleaned file saved")
 print(df.head())
