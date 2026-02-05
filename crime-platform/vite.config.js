8d274a60afd92cc902e91046ff71ce398d1a1c46
anastassiakaarma <anastassiakaarma@ut.ee>
2026-02-05 16:32:09 +0200

    Fix base url  for gitHub pages

--- crime-platform/vite.config.js
+++ crime-platform/vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/CrimeProject/',
+  base: '/Crime_Analysis_In_Europe/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
